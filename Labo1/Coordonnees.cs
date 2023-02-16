abae57c9f0e4b699cb1260acefb2d8cb349814b1
CyrilLibotteHepl <115027664+CyrilLibotteHepl@users.noreply.github.com>
2023-02-16 12:02:17 +0100

    Commit 1.1

---
+++ Labo1/Coordonnees.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo1
+{
+    internal class Coordonnees
+    {
+        private int X;
+
+        public int X
+        {
+            get { return X; }
+            set { X = value; }
+        }
+
+        private int Y;
+        public int Y
+        {
+            get { return Y; }
+            set { Y = value; }
+        }
+
+        public Coordonnees(int a, int b)
+        {
+            X = a;
+            Y = b;
+        }
+
+        public Coordonnees : this(0, 0)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return 
+        }
+    }
+}
